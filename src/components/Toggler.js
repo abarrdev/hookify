d41c50e3127230bc6fe47c5fd84bdc24190e0256
Andrea <andrea@andreabarr.dev>
2020-06-30 10:20:07 -0500

    toggle->le->toggleSunny; added toggleButterfly

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react'
 
 function Toggler() {
 	const [isSunny, setIsSunny] = useState(true)
+	const [isButterfly, setIsButterfly] = useState(false)
 
-	const toggle = () => {
+	const toggleSunny = () => {
 		setIsSunny(!isSunny)
+	}
+
+	const toggleButterfly = () => {
+		setIsButterfly(!isButterfly)
 	}
 
 	return(
 		<div>
-			<h1 onClick={toggle} style={{paddingTop:'50px'}}>
+			<h1 onClick={toggleSunny} style={{padding:'30px'}}>
 				{isSunny ? "☀️" : "☔️" }
+			</h1>
+			<h1 onClick={toggleButterfly} style={{padding:'30px'}}>
+				{isButterfly ? "🦋" : "🐛"}
 			</h1>
 		</div>
 	)
