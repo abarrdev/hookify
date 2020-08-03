d17027e2f4de758f0fe7cf170afa46f6ca975eef
Andrea <andrea@andreabarr.dev>
2020-08-02 20:22:25 -0500

    added second input to SimpleFormInput

--- src/components/SimpleFormInput.js
+++ src/components/SimpleFormInput.js
@@ -3,6 +3,7 @@ import useInputState from '../hooks/useInputState'
 
 function SimpleFormInput() {
 	const [name, updateName, resetName] = useInputState("")
+	const [last, updateLast, resetLast] = useInputState("")
 	
 	return(
 		<div>
@@ -10,6 +11,9 @@ function SimpleFormInput() {
 			<input type="text" value={name} onChange={updateName} />
 			<button onClick={resetName}>clear</button>
 			<br />
+			<h1>Good evening, {last}.</h1>
+            <input type="text" value={last} onChange={updateLast} />
+            <button onClick={resetLast}>clear</button>
 		</div>
 	)
 }
