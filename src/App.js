8122e585fdcb499aaf5e984aa1303e48228de283
Andrea <andrea@andreabarr.dev>
2020-08-07 13:58:45 -0500

    remove SimpleForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Toggler from './components/Toggler'
-// import SimpleForm from './components/SimpleForm'
 import SimpleFormInput from './components/SimpleFormInput';
 
 function App() {
   return (
     <div className="App">
       <Toggler />
-      {/* <SimpleForm /> */}
       <SimpleFormInput />
     </div>
   );
