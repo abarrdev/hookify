e921b4f33be3eb2b7f3917e949ed9991762c0806
Andrea <andrea@andreabarr.dev>
2020-08-07 14:26:48 -0500

    useEffect updates title

--- src/components/Clicker.js
+++ src/components/Clicker.js
@@ -3,6 +3,10 @@ import React, {useState, useEffect} from 'react'
 function Clicker() {
 	const [count, setCount] = useState(0);
 
+	useEffect(() => {
+		document.title = `You clicked ${count} times`
+	})
+
 	return(
 		<div style={{paddingTop:"20px"}}>
 			<button onClick={() => setCount(count + 1)}>{count} clicks</button>
@@ -11,3 +15,6 @@ function Clicker() {
 }
 
 export default Clicker
+
+
+//useEffect is called on each render - like componentDidMount, componentDidUpdate, componentWillUnmount combined
